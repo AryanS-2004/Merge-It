f0004d4b393f1e06855334f1199af19c3824f8f7
AryanS-2004 <aryan.coding01@gmail.com>
2023-08-01 14:54:35 +0530

    Removed a bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import {BrowserRouter} from "react-router-dom";
 import {ChakraProvider} from "@chakra-ui/react";
 import IssuesProvider from "./Context/IssuesProvider.jsx";
-// import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
