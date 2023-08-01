7c31c26a182bc9582b658d4e58d5483d97f56008
AryanS-2004 <aryan.coding01@gmail.com>
2023-08-01 14:42:11 +0530

    Minor bug removed

--- client/src/pages/IssuesPage.jsx
+++ client/src/pages/IssuesPage.jsx
@@ -1,13 +1,13 @@
-import {IssuesState} from "../Context/IssuesProvider.jsx";
-import {Avatar, Button, Menu, MenuButton, MenuItem, MenuList, useToast} from "@chakra-ui/react";
-import {AddIcon, ChevronDownIcon, DeleteIcon} from "@chakra-ui/icons";
-import {useNavigate} from "react-router-dom";
-import {useEffect, useState} from "react";
+import { IssuesState } from "../Context/IssuesProvider.jsx";
+import { Avatar, Button, Menu, MenuButton, MenuItem, MenuList, useToast } from "@chakra-ui/react";
+import { AddIcon, ChevronDownIcon, DeleteIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import IssueListItem from "../Components/IssueItem.jsx";
 
 const IssuesPage = () => {
-    const {user} = IssuesState();
+    const { user } = IssuesState();
     const [issues, setIssues] = useState([]);
     const [selectedLanguage, setSelectedLanguage] = useState("");
     const [repoLink1, setRepoLink1] = useState("");
@@ -75,15 +75,15 @@ const IssuesPage = () => {
             })
             const config = {
                 headers: {
-                    authorization:`Bearer ${user.token}`,
+                    authorization: `Bearer ${user.token}`,
                     "Content-type": "application/json"
                 }
             }
 
             const getIssues = await axios.get(`${import.meta.env.VITE_API_URL}/api/issues`, config);
-            if(selectedLanguage!==""){
+            if (selectedLanguage !== "") {
                 getIssues.data = getIssues.data.filter((issue) => issue.languages.includes(selectedLanguage))
-                if(getIssues.data.length===0){
+                if (getIssues.data.length === 0) {
                     toast({
                         title: `No issues for the language : ${selectedLanguage} !`,
                         status: "error",
@@ -93,7 +93,7 @@ const IssuesPage = () => {
                     })
                 }
                 setIssues(getIssues.data)
-                if(getIssues.data.length!==0){
+                if (getIssues.data.length !== 0) {
                     toast({
                         title: "Success!",
                         status: "success",
@@ -102,7 +102,7 @@ const IssuesPage = () => {
                         position: 'bottom'
                     })
                 }
-            }else{
+            } else {
                 setIssues(getIssues.data);
                 toast({
                     title: "Success!",
@@ -125,10 +125,10 @@ const IssuesPage = () => {
         }
     }
     useEffect(() => {
-        if(user){
+        if (user) {
             fetchIssues();
         }
-    },[user,selectedLanguage, fetchAgain]);
+    }, [user, selectedLanguage, fetchAgain]);
 
 
     const logoutHandler = () => {
@@ -138,20 +138,25 @@ const IssuesPage = () => {
 
     const addRepo = async () => {
 
-        if(repoLink1===''){
+        if (repoLink1 === '') {
             return;
         }
-        try{
-            const config ={
+        try {
+            const config = {
                 headers: {
                     "Content-type": "application/json",
                     authorization: `Bearer ${user.token}`
                 }
             }
-
-            await axios.put(`${import.meta.env.VITE_API_URL}/api/repos/addRepo`, {
-                link: repoLink1 +"/issues"
-            }, config);
+            if (repoLink1.endsWith('issues')) {
+                await axios.put(`${import.meta.env.VITE_API_URL}/api/repos/addRepo`, {
+                    link: repoLink1
+                }, config);
+            }else{
+                await axios.put(`${import.meta.env.VITE_API_URL}/api/repos/addRepo`, {
+                    link: repoLink1 + "/issues"
+                }, config);
+            }
             setRepoLink1("");
             setFetchAgain(!fetchAgain);
             toast({
@@ -161,7 +166,7 @@ const IssuesPage = () => {
                 isClosable: true,
                 position: 'bottom'
             })
-        }catch(err){
+        } catch (err) {
             toast({
                 title: "Error Occured!",
                 description: err.response.data,
@@ -174,10 +179,10 @@ const IssuesPage = () => {
     }
 
     const removeRepo = async () => {
-        if(repoLink2===''){
+        if (repoLink2 === '') {
             return;
         }
-        try{
+        try {
             const config = {
                 headers: {
                     "Content-type": "application/json",
@@ -199,7 +204,7 @@ const IssuesPage = () => {
                 position: 'bottom'
             })
 
-        }catch(err){
+        } catch (err) {
             toast({
                 title: "Error Occured!",
                 description: err.response.data,
@@ -214,15 +219,15 @@ const IssuesPage = () => {
     if (user) {
         return (
             <>
-                <div style={{height: "100vh", fontFamily: "Josefin Sans"}}>
+                <div style={{ height: "100vh", fontFamily: "Josefin Sans" }}>
                     <div className="flex bg-[#1A202C] text-[#E2E8F0] text-center  rounded-md py-4  justify-between">
                         <div className="pl-8 font-['Sacramento'] text-4xl font-black">
                             Merge It
                         </div>
                         <div className="pr-8">
                             <Menu>
-                                <MenuButton as={Button} rightIcon={<ChevronDownIcon/>} backgroundColor="#284b63">
-                                    <Avatar size="sm" cursor="pointer" name={user.name}/>
+                                <MenuButton as={Button} rightIcon={<ChevronDownIcon />} backgroundColor="#284b63">
+                                    <Avatar size="sm" cursor="pointer" name={user.name} />
                                 </MenuButton>
                                 <MenuList color="#1A202C">
                                     <MenuItem onClick={logoutHandler}>Logout</MenuItem>
@@ -236,39 +241,39 @@ const IssuesPage = () => {
                                 Language
                             </MenuButton>
                             <MenuList className="max-h-64 overflow-y-auto">
-                                {Languages.map((language)=> <MenuItem key={language} onClick={() => setSelectedLanguage(language)}>
-                                        {language}</MenuItem>
-                                    )}
+                                {Languages.map((language) => <MenuItem key={language} onClick={() => setSelectedLanguage(language)}>
+                                    {language}</MenuItem>
+                                )}
                             </MenuList>
                         </Menu>
                         <div>
                             <input
-                                style={{fontFamily: "Josefin Sans",}}
+                                style={{ fontFamily: "Josefin Sans", }}
                                 type="text"
                                 value={repoLink1}
                                 placeholder="Add Repository Link"
                                 onChange={(e) => setRepoLink1(e.target.value)}
                                 className="rounded-lg p-2 border-t  mr-0 border-b border-l text-gray-900 border-gray-200 bg-white"
                             />
-                                <AddIcon onClick={addRepo} boxSize={8} color = "gray.200" ml={2}/>
+                            <AddIcon onClick={addRepo} boxSize={8} color="gray.200" ml={2} />
                         </div>
                         <div>
                             <input
-                                style={{fontFamily: "Josefin Sans",}}
+                                style={{ fontFamily: "Josefin Sans", }}
                                 type="text"
                                 value={repoLink2}
                                 placeholder="Add Repository Link"
                                 onChange={(e) => setRepoLink2(e.target.value)}
                                 className="rounded-lg p-2 border-t  mr-0 border-b border-l text-gray-900 border-gray-200 bg-white"
                             />
-                            <DeleteIcon onClick={removeRepo} boxSize={8} color = "gray.200" ml={2}/>
+                            <DeleteIcon onClick={removeRepo} boxSize={8} color="gray.200" ml={2} />
                         </div>
                     </div>
                     <div className="max-h-full overflow-y-auto pb-4" style={{ scrollbarColor: "#FFF transparent" }}>
                         {
-                            issues?.map( (issue)=> <IssueListItem
+                            issues?.map((issue) => <IssueListItem
                                 key={issue.title}
-                                issue = {issue}
+                                issue={issue}
                             />)
                         }
                     </div>
