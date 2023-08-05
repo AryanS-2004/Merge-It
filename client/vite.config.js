c133de40849fa99ce19ca5055a5ee6f0abeb5401
AryanS-2004 <aryan.coding01@gmail.com>
2023-08-05 23:49:35 +0530

    Hopefully fixed the production issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["@emotion/react"],
+      external: ["@emotion/react",'@emotion/styled',],
     },
   },
 })
